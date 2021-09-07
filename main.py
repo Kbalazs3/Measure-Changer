cf69bacd5b056e0652aeea3dd6c4c899edb0481f
Kbalazs3 <kbalazs93@gmail.com>
2021-09-07 06:58:26 +0200

    m to cm changing works

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import input
 
 
 def main():
-    original = input.ask_original_measure()
+    original = input.ask_original_measure().lower()
     target = input.ask_target_measure()
     amount = input.ask_amount()
     if original == ('g' or original == "gram") and target == 'kg':
@@ -20,6 +20,9 @@ def main():
     elif original == 'kg' and target == 'dkg':
         kg_to_dkg_result = mass.kg_to_dkg(amount)
         display.display_result(kg_to_dkg_result, amount, original, target)
+    elif original == 'm' and target == 'cm':
+        m_to_cm_result = length.m_to_cm(amount)
+        display.display_result(m_to_cm_result, amount, original, target)
 
 
 if __name__ == '__main__':
