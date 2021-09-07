2dd592a199e32ebe2144404cf87ccd21b938acad
Kbalazs3 <kbalazs93@gmail.com>
2021-09-07 07:02:16 +0200

    m to cm and cm to m works and refactoring happened

--- main.py
+++ main.py
@@ -7,22 +7,26 @@ import input
 def main():
     original = input.ask_original_measure().lower()
     target = input.ask_target_measure()
-    amount = input.ask_amount()
+    amount_to_change = input.ask_amount()
+
     if original == ('g' or original == "gram") and target == 'kg':
-        gram_kg_result = mass.change_gram_to_kg(amount)
-        display.display_result(gram_kg_result, amount, original, target)
+        gram_kg_result = mass.change_gram_to_kg(amount_to_change)
+        display.display_result(gram_kg_result, amount_to_change, original, target)
     elif original == 'kg' and (target == 'g' or target == 'gram'):
-        kg_to_gram = mass.change_kg_to_gram(amount)
-        display.display_result(kg_to_gram, amount, original, target)
+        kg_to_gram = mass.change_kg_to_gram(amount_to_change)
+        display.display_result(kg_to_gram, amount_to_change, original, target)
     elif original == 'dkg' and target == 'kg':
-        dkg_to_kg_result = mass.dkg_to_kg(amount)
-        display.display_result(dkg_to_kg_result, amount, original, target)
+        dkg_to_kg_result = mass.dkg_to_kg(amount_to_change)
+        display.display_result(dkg_to_kg_result, amount_to_change, original, target)
     elif original == 'kg' and target == 'dkg':
-        kg_to_dkg_result = mass.kg_to_dkg(amount)
-        display.display_result(kg_to_dkg_result, amount, original, target)
+        kg_to_dkg_result = mass.kg_to_dkg(amount_to_change)
+        display.display_result(kg_to_dkg_result, amount_to_change, original, target)
     elif original == 'm' and target == 'cm':
-        m_to_cm_result = length.m_to_cm(amount)
-        display.display_result(m_to_cm_result, amount, original, target)
+        m_to_cm_result = length.m_to_cm(amount_to_change)
+        display.display_result(m_to_cm_result, amount_to_change, original, target)
+    elif original == 'cm' and target == 'm':
+        cm_to_m_result = length.m_to_cm(amount_to_change)
+        display.display_result(cm_to_m_result, amount_to_change, original, target)
 
 
 if __name__ == '__main__':
