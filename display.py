d2a1f53fa61a95de9cf88437c7636a57b0943680
Kbalazs3 <kbalazs93@gmail.com>
2021-09-10 18:20:49 +0200

    UI improvements, refactoring

--- display.py
+++ display.py
@@ -1,16 +1,22 @@
 
 
+def to_middle():
+    return "\t\t\t\t\t\t\t\t"
+
+
 def display_result(result, amount, original_measure, target_measure):
-    print(str(amount) + original_measure + " = " + str(float(result)) + target_measure)
+    middle = to_middle()
+    print("\n" + 2 * middle + str(amount) + original_measure + " = " + str(float(result)) + target_measure)
 
 
 def start_menu_display():
-    to_middle = "\t\t\t\t\t\t\t\t"
-    print(2 * to_middle +
-          "Measure Changer" + "\n\n" + 2 * to_middle + "START MENU" + "\n\n"+ 2 * to_middle + "Start -> Press S" + "\n"
-          + 2 * to_middle + "Description -> Press D" + "\n"
-          + 2 * to_middle + "Quit -> Press Q" + "\n\n")
+    middle = to_middle()
+    print("\n" + 2 * middle +
+          "Measure Changer" + "\n\n" + 2 * middle + "START MENU" + "\n\n"+ 2 * middle + "Start -> Press S" + "\n"
+          + 2 * middle + "Description -> Press D" + "\n"
+          + 2 * middle + "Quit -> Press Q" + "\n\n")
 
 
 def program_description_display():
-    print("\n\n\nThis program can change, masses, lengths, time, speed\n\n")
+    middle = to_middle()
+    print("\n\n\n" + middle + "This program can change, masses, lengths, time, speed\n\n")
--- input.py
+++ input.py
@@ -1,24 +1,25 @@
 
+
 def ask_amount():
-    amount = input("Enter the amount to change: ")
+    amount = input("\t\t\t\t\t\t\t\t\t\tEnter the amount to change: ")
     return amount
 
 
 def ask_original_measure():
-    or_measure = input("Please enter the original measure: ")
+    or_measure = input("\t\t\t\t\t\t\t\t\t\tPlease enter the original measure: ")
     return or_measure
 
 
 def ask_target_measure():
-    target_measure = input("Please enter the target measure: ")
+    target_measure = input("\t\t\t\t\t\t\t\t\t\tPlease enter the target measure: ")
     return target_measure
 
 
 def ask_star_menu_command():
-    start_menu_command = input("\t\t\t\t\t\t\t\tStart Menu command: ")
+    start_menu_command = input("\t\t\t\t\t\t\t\t\t\tStart Menu command: ")
     return start_menu_command
 
 
 def back_to_main_menu():
-    back_to_menu = input('You can step back to the main menu by entering "back": ').lower()
+    back_to_menu = input('\t\t\t\t\t\t\t\t\t\tYou can step back to the main menu by entering "back": ').lower()
     return back_to_menu
